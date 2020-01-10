97f4d21be9c75d843384017c59099965a77f18bd
vquanghuy <vquanghuy@gmail.com>
2020-01-10 09:53:03 +0700

    - Update tank

---
+++ Assets/_Main/Scripts/MyTankMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTankMovement : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float turnSpeed = 40;
+
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+        Move();
+        Turn();
+    }
+
+    void Move() {
+        float v = Input.GetAxisRaw("Vertical1");
+        Vector3 offset = transform.forward * moveSpeed * Time.deltaTime * v;
+
+        rigidbody.MovePosition(transform.position + offset);
+    }
+
+    void Turn() {
+        float h = Input.GetAxisRaw("Horizontal1");
+        
+        Vector3 currentRotation = transform.rotation.eulerAngles;
+        currentRotation.y += turnSpeed * Time.deltaTime * h;
+
+        rigidbody.MoveRotation(Quaternion.Euler(currentRotation));
+    }
+}
